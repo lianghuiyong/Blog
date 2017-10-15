a20771a43b1e4564639900f9f2b7333d8be8d3aa
梁惠涌 <lianghuiyong@outlook.com>
2017-10-16 01:05:25 +0800

    up data

--- blog_server/bin/start.sh
+++ blog_server/bin/start.sh
@@ -5,11 +5,11 @@ if [ $? -ne 0 ]
 then
   sudo nginx  -t -c /Users/huiyong/Documents/project/blog_server/config/nginx.conf
   sudo nginx  -c /Users/huiyong/Documents/project/blog_server/config/nginx.conf
-  echo "nginx start"
+  echo "nginx: start"
 else
   sudo nginx  -t -c /Users/huiyong/Documents/project/blog_server/config/nginx.conf
   sudo nginx  -s reload -c /Users/huiyong/Documents/project/blog_server/config/nginx.conf
-  echo "nginx reload"
+  echo "nginx: reload"
 fi
 echo -e "===========================================\n\n"
-tail -f /usr/local/Cellar/openresty/1.11.2.5/nginx/logs/error.log
+sudo tail -f /Users/huiyong/Documents/project/blog_server/logs/error.log
--- blog_server/bin/stop.sh
+++ blog_server/bin/stop.sh
@@ -3,6 +3,5 @@
 sudo nginx  -t -c /Users/huiyong/Documents/project/blog_server/config/nginx.conf
 sudo nginx  -s quit -c /Users/huiyong/Documents/project/blog_server/config/nginx.conf
 
-echo "nginx stop"
+echo "nginx: stop"
 echo -e "===========================================\n\n"
-tail -f /usr/local/Cellar/openresty/1.11.2.5/nginx/logs/error.log
