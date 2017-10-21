04267b4adaa6011feec4d68aa0bb441b619dce73
梁惠涌 <lianghuiyong@outlook.com>
2017-10-21 20:20:39 +0800

    up

--- blog_server/bin/start.sh
+++ blog_server/bin/start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # 将api目录及其子目录皆设为任何人可执行
-sudo chmod -R 777 /Users/huiyong/Documents/project/blog_web/*
+sudo chmod -R 777 /root/blog/blog_web/*
 
 ps -fe|grep nginx |grep -v grep
 if [ $? -ne 0 ]
