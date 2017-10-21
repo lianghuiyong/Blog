800ddbe0b1e26241eac73112d3d6ad389f2b68b5
梁惠涌 <lianghuiyong@outlook.com>
2017-10-21 20:22:39 +0800

    up

--- blog_server/bin/start.sh
+++ blog_server/bin/start.sh
@@ -6,12 +6,12 @@ sudo chmod -R 777 /root/blog/blog_web/*
 ps -fe|grep nginx |grep -v grep
 if [ $? -ne 0 ]
 then
-  sudo nginx  -t -c /root/blog/blog_server/config/nginx.conf
-  sudo nginx  -c /root/blog/blog_server/config/nginx.conf
+  nginx  -t -c /root/blog/blog_server/config/nginx.conf
+  nginx  -c /root/blog/blog_server/config/nginx.conf
   echo "nginx: start"
 else
-  sudo nginx  -t -c /root/blog/blog_server/config/nginx.conf
-  sudo nginx  -s reload -c /root/blog/blog_server/config/nginx.conf
+  nginx  -t -c /root/blog/blog_server/config/nginx.conf
+  nginx  -s reload -c /root/blog/blog_server/config/nginx.conf
   echo "nginx: reload"
 fi
 echo -e "===========================================\n\n"
