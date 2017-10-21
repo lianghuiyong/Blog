aae1a981b17da2ca122f7bed5dbc13337d50e644
梁惠涌 <lianghuiyong@outlook.com>
2017-10-21 20:17:47 +0800

    up to centos

--- blog_server/bin/start.sh
+++ blog_server/bin/start.sh
@@ -6,13 +6,13 @@ sudo chmod -R 777 /Users/huiyong/Documents/project/blog_web/*
 ps -fe|grep nginx |grep -v grep
 if [ $? -ne 0 ]
 then
-  sudo nginx  -t -c /Users/huiyong/Documents/project/blog_server/config/nginx.conf
-  sudo nginx  -c /Users/huiyong/Documents/project/blog_server/config/nginx.conf
+  sudo nginx  -t -c /root/blog/blog_server/config/nginx.conf
+  sudo nginx  -c /root/blog/blog_server/config/nginx.conf
   echo "nginx: start"
 else
-  sudo nginx  -t -c /Users/huiyong/Documents/project/blog_server/config/nginx.conf
-  sudo nginx  -s reload -c /Users/huiyong/Documents/project/blog_server/config/nginx.conf
+  sudo nginx  -t -c /root/blog/blog_server/config/nginx.conf
+  sudo nginx  -s reload -c /root/blog/blog_server/config/nginx.conf
   echo "nginx: reload"
 fi
 echo -e "===========================================\n\n"
-sudo tail -f /Users/huiyong/Documents/project/blog_server/logs/error.log
+sudo tail -f /root/blog/blog_server/logs/error.log
--- blog_server/bin/stop.sh
+++ blog_server/bin/stop.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-sudo nginx  -t -c /Users/huiyong/Documents/project/blog_server/config/nginx.conf
-sudo nginx  -s quit -c /Users/huiyong/Documents/project/blog_server/config/nginx.conf
+sudo nginx  -t -c /root/blog/blog_server/config/nginx.conf
+sudo nginx  -s quit -c /root/blog/blog_server/config/nginx.conf
 
 echo "nginx: stop"
 echo -e "===========================================\n\n"
