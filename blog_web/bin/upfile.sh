5aff1e85fe679d4f8cdc92783a75c341cc6b821d
梁惠涌 <lianghuiyong@outlook.com>
2017-10-24 18:18:56 +0800

    up web

--- blog_web/bin/upfile.sh
+++ blog_web/bin/upfile.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-scp /Users/huiyong/Documents/project/blog_web/index.html root@120.78.153.171:/root/blog_server/web
-scp -r /Users/huiyong/Documents/project/blog_web/dist root@120.78.153.171:/root/blog_server/web
+scp -r /Users/huiyong/Documents/project/blog_web/index.html  /Users/huiyong/Documents/project/blog_web/dist root@120.78.153.171:/root/blog_server/web
 
 echo -e "\n==========================================="
 echo "up: web!"
